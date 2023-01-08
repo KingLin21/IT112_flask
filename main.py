6426f5e3a16d1a1eda5c43149888892f403d8060
KingLin21 <117612761+KingLin21@users.noreply.github.com>
2023-01-08 23:18:49 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Hello from Flask!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 app.run(host='0.0.0.0', port=81)
