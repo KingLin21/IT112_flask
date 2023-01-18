1423589676d6f9404fe7d506c01392d08d6c6824
KingLin21 <117612761+KingLin21@users.noreply.github.com>
2023-01-18 02:32:10 +0000

    fixed fortunetold.html

--- main.py
+++ main.py
@@ -18,52 +18,59 @@ def fortune():
 
 @app.route('/fortunetold', methods=['GET','POST'])
 def fortunetold():
+  fortune=None
   if request.method == 'POST':
-    name = request.form.get('user')
     color = request.form.get('color')
     number = request.form.get('number')
     if color == 'red' and number =='1':
-      return "Hello "+name+", your fortune is... You will have great success in life"
+      fortune = "You will have great success in life"
     elif color =='red' and number =='2':
-      return "Hello "+name+", your fortune is... You will be unusually successful in business"
+      fortune = "You will be unusually successful in business"
     elif color =='red' and number =='3':
-      return "Hello "+name+", your fortune is... You will become more and more wealthy"
+      fortune = "You will become more and more wealthy"
     elif color =='red' and number =='4':
-      return "Hello "+name+", your fortune is... You will make a change for the better"
+      fortune = "You will make a change for the better"
     elif color =='red' and number =='5':
-      return "Hello "+name+", your fortune is... You will be loved by millions"
+      fortune = "You will be loved by millions"
     elif color =='yellow' and number =='1':
-      return "Hello "+name+", your fortune is... You will travel all around the world"
+      fortune = "You will travel all around the world"
     elif color =='yellow' and number =='2':
-      return "Hello "+name+", your fortune is... You will alwyas be surrounded by true friends"
+      fortune = "You will alwyas be surrounded by true friends"
     elif color =='yellow' and number =='3':
-      return "Hello "+name+", your fortune is... You will be find many treasures in life"
+      fortune = "You will be find many treasures in life"
     elif color =='yellow' and number =='4':
-      return "Hello "+name+", your fortune is... You are blessed with a long life"
+      fortune = "You are blessed with a long life"
     elif color =='yellow' and number =='5':
-      return "Hello "+name+", your fortune is... You will enjoy good health"
+      fortune = "You will enjoy good health"
     elif color =='blue' and number =='1':
-      return "Hello "+name+", your fortune is... You will inherit a large sum of money"
+      fortune = "You will inherit a large sum of money"
     elif color =='blue' and number =='2':
-      return "Hello "+name+", your fortune is... A life time of happiness awaits you"
+      fortune = "A life time of happiness awaits you"
     elif color =='blue' and number =='3':
-      return "Hello "+name+", your fortune is... A beautiful, smart, and loving person will enter your life"
+      fortune = "A beautiful, smart, and loving person will enter your life"
     elif color =='blue' and number =='4':
-      return "Hello "+name+", your fortune is... A dubious friend may be an enemy in hiding"
+      fortune = "A dubious friend may be an enemy in hiding"
     elif color =='blue' and number =='5':
-      return "Hello "+name+", your fortune is... A pleasant surprise awaits you"
+      fortune = "A pleasant surprise awaits you"
     elif color =='green' and number =='1':
-      return "Hello "+name+", your fortune is... All will go well with your next project"
+      fortune = "All will go well with your next project"
     elif color =='green' and number =='2':
-      return "Hello "+name+", your fortune is... All your hardwork will soon pay off"
+      fortune = "All your hardwork will soon pay off"
     elif color =='green' and number =='3':
-      return "Hello "+name+", your fortune is... Allow compassion to guide your decisions"
+      fortune = "Allow compassion to guide your decisions"
     elif color =='green' and number =='4':
-      return "Hello "+name+", your fortune is... Believe in yourself and others will too"
+      fortune = "Believe in yourself and others will too"
     elif color =='green' and number =='5':
-      return "Hello "+name+", your fortune is... Bide your time, for success is near"
-  return render_template('fortunetold.html')
+      fortune = "Bide your time, for success is near"
+  return render_template('fortunetold.html', user=request.form.get('user'), fortune=fortune)
 
-
+@app.route('/schedule/<student_name>')
+def schedule(student_name):
+  courses = [
+    {"id": 123, "name": "IT 121"},
+    {"id": 456, "name": "IT 276"},
+    {"id": 789, "name": "MATH 140"},
+  ]
+  return render_template('Test.html', student_name=student_name, courses=courses)
   
 app.run(host='0.0.0.0', port=81)
